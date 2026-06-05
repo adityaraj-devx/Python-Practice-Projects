20f5e8bc8a32f398fe3386956678c83b018d4d37
adityaraj-devx <aadiiraj7780@gmail.com>
2026-06-05 18:50:31 +0530

    completed

---
+++ Alarm Clock (easy)/main.py
@@ -0,0 +1,33 @@
+import winsound
+import time
+
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\033[H"
+
+def alarm(seconds):
+    time_elapsed = 0
+
+    print(CLEAR)
+    while time_elapsed < seconds:
+        time.sleep(1)
+        time_elapsed += 1
+
+        time_left = seconds - time_elapsed
+        minutes_left = time_left // 60
+        seconds_left = time_left % 60
+
+        print(f"{CLEAR_AND_RETURN}Alarm will ring in: {minutes_left:02d}:{seconds_left:02d}")
+
+    winsound.PlaySound("Alarm Clock (easy)/alarm.wav", winsound.SND_FILENAME)
+
+while True:
+    try:
+        minutes = int(input("How many minutes to wait: "))
+        secondss = int(input("How many seconds to wait: "))
+        total_time = minutes * 60 + secondss
+        break
+    except ValueError:
+        print("Invalid input. Enter a number!")
+
+
+alarm(total_time)
