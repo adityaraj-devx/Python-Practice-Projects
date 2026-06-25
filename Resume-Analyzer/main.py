1691e577202e5dc7a4fe603fa30b7a0695fade94
adityaraj-devx <aadiiraj7780@gmail.com>
2026-06-25 22:39:08 +0530

    updated

---
+++ Resume-Analyzer/main.py
@@ -0,0 +1,13 @@
+import io
+import os 
+import PyPDF2
+from groq import Groq
+import streamlit as stm
+from dotenv import load_dotenv
+
+load_dotenv()
+
+stm.set_page_config(page_title="Resume Analyzer", page_icon="🗒️", layout="centered")
+
+stm.title("Resume Analyzer")
+stm.markdown("Upload to get an AI-powered analysis of your resume.")
